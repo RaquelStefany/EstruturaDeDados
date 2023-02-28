0af227b6494b78cc3e5d737a5bf688f6ddc142a6
RaquelStefany <raquelstefany.s@hotmail.com>
2023-02-28 15:06:28 -0300

    Finalizado

---
+++ Exerc01/Exerc01.cpp
@@ -0,0 +1,45 @@
+// Exerc01.cpp : Este arquivo contém a função 'main'. A execução do programa começa e termina ali.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double nota1 = 0, nota2 = 0, media = 0;
+    string nome;
+
+    cout << "Digite seu nome: ";
+    cin >> nome;
+
+    do {
+        cout << "Digite a 1 nota: ";
+        cin >> nota1;
+    } while (nota1 < 0);
+
+    do {
+        cout << "Digite a 2 nota: ";
+        cin >> nota2;
+    } while (nota2 < 0);
+
+    media = (nota1 + nota2) / 2;
+    cout << "\nMedia: " << media;
+
+    if (media >= 6) {
+        cout << "\nAluno Aprovado\n";
+    }
+    else {
+        cout << "\nAluno Reprovado\n";
+    }
+}
+
+// Executar programa: Ctrl + F5 ou Menu Depurar > Iniciar Sem Depuração
+// Depurar programa: F5 ou menu Depurar > Iniciar Depuração
+
+// Dicas para Começar: 
+//   1. Use a janela do Gerenciador de Soluções para adicionar/gerenciar arquivos
+//   2. Use a janela do Team Explorer para conectar-se ao controle do código-fonte
+//   3. Use a janela de Saída para ver mensagens de saída do build e outras mensagens
+//   4. Use a janela Lista de Erros para exibir erros
+//   5. Ir Para o Projeto > Adicionar Novo Item para criar novos arquivos de código, ou Projeto > Adicionar Item Existente para adicionar arquivos de código existentes ao projeto
+//   6. No futuro, para abrir este projeto novamente, vá para Arquivo > Abrir > Projeto e selecione o arquivo. sln
---
+++ Exerc01/readme.md
@@ -0,0 +1,16 @@
+Elabore um programa em <strong>C++</strong> que peça os seguintes dados:
+<br>
+<ul>
+    <li>
+        Nome
+    </li>
+    <li>
+        Nota da P1
+    </li>
+    <li>
+        Nota da P2
+    </li>
+</ul>
+A partir dos dados digitados calcule e exiba a média aritmética.
+<br>
+Caso a  média seja maior ou igual a 6 exiba APROVADO caso contrário exiba REPROVADO.
